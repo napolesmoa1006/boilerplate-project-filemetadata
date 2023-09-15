dcc45d26d01ce2c586163b91a68f09b31bf7ea43
Carlos Napoles Avila <napolesmoa1006@gmail.com>
2023-09-14 22:02:13 -0400

    Initial config

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -16,5 +19,5 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on  http://localhost:' + port)
 });
